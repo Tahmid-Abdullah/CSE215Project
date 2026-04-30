d3a0b46ce3f222063ab9e38bf9ce0616df121e7e
[Tahmid-Abdullah] <[tahmid.abdullah.242@northsouth.edu]>
2026-04-30 20:20:44 +0600

    Tahmid er commit

--- src/main/java/pkg/java/project/transfermarket/UI/HelloApplication.java
+++ src/main/java/pkg/java/project/transfermarket/UI/HelloApplication.java
@@ -10,6 +10,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FileManager.FileInitialization();
+        // Load in-memory lists from files so UI controllers can read data immediately
+        Lists.syncAllFiles();
         SceneManager.getInstance().setPrimaryStage(stage);
         SceneManager.getInstance().showLoginScene();
     }
